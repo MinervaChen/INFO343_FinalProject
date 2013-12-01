f8e7fd7028db77e3afd69a493be67f1de23d3e4c
James Prow <jkprow@gmail.com>
2013-12-01 14:38:39 -0800

    Dynamically rendered volunteers page
    Added MVC in JS that reads volunteer JSON data (json/volunteers.json)
    and renders it dynamically on the volunteers.html page.
    Minimal styling applied.

---
+++ js/controller.js
@@ -0,0 +1,24 @@
+/* controller.js
+	Controller for directory.html
+*/
+
+$(function(){
+	var directoryModel = createDirectoryModel({
+		url: 'json/volunteers.json'
+	});
+
+	var directoryView = createDirectoryView({
+		model: directoryModel,
+		template: $('.volunteer-template'),
+		container: $('.voluneers-container')
+	});
+
+	// refresh to get new volunteers from server
+	directoryModel.refresh();
+
+	// when the directory view triggers 'moreInfo'
+	// open volunteer's information page
+	directoryView.on('volunteerInfo', function(data) {
+		alert("This currently does nothing!");
+	});
+});
---
+++ js/volunteerView.js
@@ -0,0 +1,24 @@
+/*
+    createVolunteerView()
+
+    Factory for a view that can render a given movie model.
+    Uses TemplateView as its prototype and overrides render()
+    to create the add to cart buttons for the various formats.
+
+    Note: this should probably be done with a nested view/model
+    but this works for now.
+*/
+
+function createVolunteerView(config) {
+	var view = createTemplateView(config);
+
+	view.afterRender = function(clonedTemplate) {
+		// add alt attr to pic
+		clonedTemplate.find('.pic').attr('alt', 'Photo of ' + this.model.name);
+
+		// add More Info button
+		clonedTemplate.find('.more-info').attr('data-volunteer-id', this.model.id);
+	};
+	
+	return view;
+}
