e225f4a8b2962e87624270cb3a674bcc2879138f
James Prow <jkprow@gmail.com>
2013-12-03 21:31:30 -0800

    James edit
    Right float nav, begin adding JS for volunteer editing

--- js/directoryView.js
+++ js/directoryView.js
@@ -5,18 +5,24 @@
     directory model. Uses TemplateListView as prototype.
     Overrides render() in order to add event handlers for
     the add to cart buttons.
-*/
+    */
 
-function createDirectoryView(config) {
-	config.templateView = createVolunteerView(config);
-	var view = createTemplateListView(config);
+    function createDirectoryView(config) {
+    	config.templateView = createVolunteerView(config);
+    	var view = createTemplateListView(config);
 
-	view.afterRender = function() {
+    	view.afterRender = function() {
 		// add event handlers for more info button
 		this.container.find('.more-info').click(function(){
 			var button = $(this);
 			var eventData = button.attr('data-volunteer-id');
 			view.trigger('volunteerInfo', eventData);
+		});
+
+		this.container.find('.edit-submit').click(function(){
+			var button = $(this);
+			var eventData =  button.attr('data-volunteer-id');
+			view.trigger('volunteerEdit', eventData);
 		});
 	};// afterRender()
 
